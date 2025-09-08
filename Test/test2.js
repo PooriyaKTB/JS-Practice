f11995d6c1519aae58814e4f268672cecf8d8cb0
Pooriya Ketabi <159670309+PooriyaKTB@users.noreply.github.com>
2025-09-08 22:36:40 +0100

    182-2

--- Test/test2.js
+++ Test/test2.js
@@ -1,10 +1,18 @@
 const obje = {
-    firstName: 'Pooriya',
-    lastName: 'ketabi',
-    birthDate: 1992,
-    job: ['SE','chef']
-}
+  firstName: "Pooriya",
+  lastName: "ketabi",
+  birthDate: 1992,
+  job: ["SE", "chef"],
+};
 
-const objMap = new Map(Object.entries(obje))
+const objMap = new Map(Object.entries(obje));
 console.log(objMap);
-console.log(objMap.get('job'));
+console.log(objMap.get("job"));
+
+// ------------------------------------- Section 12 - 182 ----------------------------------------------
+
+const createRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+console.log(createRandomInt(20, 25));
