b0eb9c1594100ce54c8b9c33174fcfb621899ea8
Pooriya Ketabi <159670309+PooriyaKTB@users.noreply.github.com>
2025-09-10 20:43:19 +0100

    186

--- Test/test2.js
+++ Test/test2.js
@@ -23,6 +23,28 @@ const isEven = (x) => x % 2 === 0;
 
 // ------------------------------------- Section 12 - 184 ----------------------------------------------
 
-console.log(2 ** 53 - 1); 
+console.log(2 ** 53 - 1);
 console.log(90071992547409953); // more that 2**53 cause wierd result
 console.log(Number.MAX_SAFE_INTEGER);
+
+// ------------------------------------- Section 12 - 186 ----------------------------------------------
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+// console.log(future.getYear()); //Don't use this
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime()); // returns date as timestamp (milliseconds from beginning og Unix time (January 1 1970))
+console.log(new Date(2142256980000)); //result is same as our date
+
+console.log(Date.now()); //returns current time as timestamp (based on PC time)
+
+// We have set versions of All methods above (.set...()) to set into our Date (not get)
+console.log(future.setFullYear(2040));
+console.log(future);
