b641541917b9389dd60d4c67f0dc23858712adc3
Pooriya <pooriya.ketabi@gmail.com>
2025-10-30 11:48:39 +0000

    221-4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -3,6 +3,10 @@ const Person = function(firstName, age){  // Constructor Function.  Constructor 
     // instance properties
     this.firstName = firstName;           // Function declaration also works for Constractor function as function expression (as used here) BUT arrow function does NOT work (mainly because of "this" keyword).
     this.age = age
+
+    this.birthYear = function(){    // This is very bad practice, we should never create a method inside constructor function. because of performance while we creat lots of instances. instead we use prototype and prototype inheritance
+        console.log(2025 - this.age)
+    }
 }
 
 const me = new Person('pooriya', 33)     
@@ -15,3 +19,5 @@ console.log(me);
 const me2 = "Person"
 console.log(me instanceof Person);
 console.log(me2 instanceof Person);
+
+me.birthYear()
