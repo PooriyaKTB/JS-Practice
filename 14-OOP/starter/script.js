5ee5f01458e9e839938be905a8a4394e21ff06d8
Pooriya <pooriya.ketabi@gmail.com>
2025-10-31 13:21:39 +0000

    225

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,44 +1,79 @@
 'use strict';
-const Person = function(firstName, age){  // Constructor Function.  Constructor functions name always start with Capital letter & always called with "new" operator.
-    // instance properties
-    this.firstName = firstName;           // Function declaration also works for Constractor function as function expression (as used here) BUT arrow function does NOT work (mainly because of "this" keyword).
-    this.age = age
-/* 
+const Person = function (firstName, age) {
+  // Constructor Function.  Constructor functions name always start with Capital letter & always called with "new" operator.
+  // instance properties
+  this.firstName = firstName; // Function declaration also works for Constractor function as function expression (as used here) BUT arrow function does NOT work (mainly because of "this" keyword).
+  this.age = age;
+  /* 
     this.birthYear = function(){    // This is very bad practice, we should never create a method inside constructor function. because of performance while we creat lots of instances. instead we use prototype and prototype inheritance
         console.log(2025 - this.age)
     } 
 */
-}
+};
 
-Person.prototype.birthYear = function(){
-    console.log(2025-this.age)
-}
+Person.prototype.birthYear = function () {
+  console.log(2025 - this.age);
+};
 
 console.log(Person.prototype);
 
 console.log('-----------');
 
-
-const me = new Person('pooriya', 33)     
-// What happend when we call a Constractor function with "new" operator: 
-// 1- new empty Object will create. => {}  
-    // 2- function is called, "this" keyword will set to newly created object. => this = {}  
-    // 3- newly created object linked to a prototype. => {} linked to prototype. 
-    // 4- newly created object will automatically return from constructor function. => function automatically return {} (not empty anymore) 
-    console.log(me);
-    const me2 = "Person"
-    console.log(me instanceof Person);
-    console.log(me2 instanceof Person);
-    
+const me = new Person('pooriya', 33);
+// What happend when we call a Constractor function with "new" operator:
+// 1- new empty Object will create. => {}
+// 2- function is called, "this" keyword will set to newly created object. => this = {}
+// 3- newly created object linked to a prototype. => {} linked to prototype.
+// 4- newly created object will automatically return from constructor function. => function automatically return {} (not empty anymore)
+console.log(me);
+const me2 = 'Person';
+console.log(me instanceof Person);
+console.log(me2 instanceof Person);
 
 // me.birthYear()
-   
- me.birthYear()
+
+me.birthYear();
 
 console.log(me.__proto__); // __proto__ property comes from step 3 on calling constructor function via new keyword. and it sets its value to the prototype property of the function that is being callled.
-console.log(me.__proto__ === Person.prototype); // NOTE: Person.prototype is not actually thr prototype of person. Butt instead it is what's gonna be used as the protoype of all the objects that are created with the Person constructor function.  
+console.log(me.__proto__ === Person.prototype); // NOTE: Person.prototype is not actually thr prototype of person. Butt instead it is what's gonna be used as the protoype of all the objects that are created with the Person constructor function.
 console.log(Person.prototype.isPrototypeOf(me)); // NOTE: to make it easier to understand, think of the prototype property as prototype of linked object property. (eg: .prototypeOfLinkedObject)
-Person.prototype.gen = 'adult'
+Person.prototype.gen = 'adult';
 
 console.log(me.hasOwnProperty('firstName'));
 console.log(me.hasOwnProperty('gen'));
+
+// ------------------------------------- Section 14 - 224 ----------------------------------------------
+
+console.log(me.__proto__);
+console.log(me.__proto__.__proto__);
+console.log(me.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+// ------------------------------------- Section 14 - 225 ----------------------------------------------
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(this.speed);
+};
+
+const Bmw = new Car('BMW', 120);
+const Benz = new Car('Benz', 95);
+
+console.log(Bmw);
+console.log(Benz);
+console.log(Bmw.accelerate());
+console.log(Bmw.brake());
+console.log(Bmw.brake());
+console.log(Bmw.brake());
